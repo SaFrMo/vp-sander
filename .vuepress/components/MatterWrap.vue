e067793620edde1909d7ea70b7b6b82e36de12a6
Sander Moolin <sandermoolin@gmail.com>
2020-09-19 15:24:17 -0400

    random shapes and mouse interaction

--- .vuepress/components/MatterWrap.vue
+++ .vuepress/components/MatterWrap.vue
@@ -6,32 +6,67 @@
 import matter from './mixins/matter'
 import { Bodies } from 'matter-js'
 // import FullCanvas from '@fuzzco/full-canvas'
+import { wait } from './libs/utils'
 
 export default {
     mixins: [matter],
     async mounted(canvas) {
         await this.$nextTick()
+
+        const width = this.$el.offsetWidth
+        const height = this.$el.offsetHeight
+
         // create a matter world and add walls to it
         this.matterInit({
             render: {
                 element: this.$el,
                 options: {
-                    width: this.$el.offsetWidth,
-                    height: this.$el.offsetHeight
+                    width,
+                    height,
+                    wireframe: false
                 }
             },
             clear: false
         })
         this.matterWalls()
+        this.matterMouse()
 
-        const body = Bodies.rectangle(
-            // width and height are computed props that return matterInit's created canvas's width and height
-            this.width / 2,
-            this.height / 2,
-            50,
-            50
-        )
-        this.matterAdd(body)
+        for (let i = 0; i < 100; i++) {
+            const dice = Math.random() * 2
+            let body
+            if (dice <= 1) {
+                body = Bodies.circle(
+                    Math.random() * width,
+                    height * 0.2 * Math.random(),
+                    Math.random() * 20 + 10,
+                    {
+                        restitution: Math.random() + 0.3,
+                        render: {
+                            fillStyle: 'red',
+                            strokeStyle: 'blue',
+                            lineWidth: 3
+                        }
+                    }
+                )
+            } else if (dice <= 2) {
+                body = Bodies.rectangle(
+                    Math.random() * width,
+                    height * 0.2 * Math.random(),
+                    Math.random() * 20 + 10,
+                    Math.random() * 20 + 10,
+                    {
+                        restitution: Math.random() + 0.3,
+                        render: {
+                            fillStyle: 'red',
+                            strokeStyle: 'blue',
+                            lineWidth: 3
+                        }
+                    }
+                )
+            }
+            this.matterAdd(body)
+            await wait(Math.random() * 2000)
+        }
     }
 }
 </script>
